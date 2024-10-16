4cea63c8f27478d283413baef8ddd693e4901910
POPCHST <dusita.siit@gmail.com>
2024-10-16 16:59:34 +0700

    pop-pdf

--- src/component/ManageUser/Signup.tsx
+++ src/component/ManageUser/Signup.tsx
@@ -1,10 +1,30 @@
+import "./Signup.css";
 
-function Signup() {
+const Signup = () => {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="sign-container">
+      <div className="sign-left-section">
+        <div className="sign-overlay">
+          {/* <p className="sign-quote">Look first / Then leap.</p>
+          <p className="sign-author">Alex Honnold</p> */}
+        </div>
+      </div>
+      <div className="sign-right-section">
+        <h2 className="lb-sign-up">ลงทะเบียน</h2>
+        <button className="sign-google-btn">ลงชื่อเข้าใช้ด้วย Google</button>
+        {/* <div className="sign-other-options">
+          <button>Facebook</button>
+          <button>Apple</button>
+          <button>LinkedIn</button>
+        </div> */}
+        <p>หรือ</p>
+        <button className="sign-email-btn">อีเมล์</button>
+        <p className="sign-login-text">
+          มีบัญชีอยู่แล้วใช่ไหม? <a href="#">Sign in</a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
